f93779205d57370a6fe9b41c7175e7f8fda65ca0
Alexander Tsebenko <sas@mbh.mbhtech>
2016-02-21 09:58:28 +0600

    Add_new_method_wich_find_duplicates

--- src/MainClass.java
+++ src/MainClass.java
@@ -18,17 +18,18 @@ public class MainClass {
 	    System.out.println(separateWordsInText(s));
 	    System.out.println(countOfWords(separateWordsInText(s)));
 	    System.out.println(wordsSetInText(s));
+	    findDuplicate(s);
 	    
 
 	}
 	static void prt(String _s) {
 	    System.out.println(_s);
 	}
-	static ArrayList separateWordsInText(String _s) {
+	static ArrayList<String> separateWordsInText(String _s) {
 	    ArrayList<String> a = new ArrayList<String>();
 	    String w = "";
 	    for(int i = 0; i < _s.length(); i++) {
-		if(isSeparateChar(_s.charAt(i))) {
+		if(isSeparateChar(_s.charAt(i)) && w != "") {
 			a.add(w);
 			w = "";
 		}
@@ -38,19 +39,19 @@ public class MainClass {
 	    }
 	    return a;
 	}
-	static Map countOfWords(ArrayList _a) {
+	static Map<String, Integer> countOfWords(ArrayList<String> _a) {
 	    Map<String, Integer> a = new HashMap<String, Integer>();
 	    Integer c = 1;
 
 	    for(int i = 0; i < _a.size(); i++) {
-		Integer wordCounter = a.put((String)_a.get(i), c);
+		Integer wordCounter = a.put(_a.get(i), c);
 		if(wordCounter != null) {
-		    a.put((String)_a.get(i), ++wordCounter);
+		    a.put(_a.get(i), ++wordCounter);
 		}
 	    }
 	    return a;
 	}
-	static Set wordsSetInText(String _s) {
+	static Set<String> wordsSetInText(String _s) {
 	    Set<String> sw = new HashSet<>();
 	    String w = "";
 	    for(int i = 0; i < _s.length(); i++) {
@@ -69,4 +70,13 @@ public class MainClass {
 		return true;
 	    return false;
 	}
+	static void findDuplicate(String _s) {
+	    ArrayList<String> a = separateWordsInText(_s);
+	    Set<String> sw = new HashSet<>();
+	    for(int i = 0; i < a.size(); i++) {
+		if(!sw.add(a.get(i)))
+		    System.out.println("Duplicate: " + a.get(i));
+	    }
+	}
+
 }
